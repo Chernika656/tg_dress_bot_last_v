24cc9162376717da3734e12f593340a513e24e37
Chernika656 <levkutepov2008@gmail.com>
2025-02-19 10:35:59 +0300

    db.py

--- db.py
+++ db.py
@@ -1,14 +1,14 @@
-import pymysql
+retimport pymysql
 
 
 try:
     connection = pymysql.connect(host='server121.hosting.reg.ru',
                             user='u0114376_project',
-                            password='',
+                            password='secret',
                             database='u0114376_project1',
                             cursorclass=pymysql.cursors.DictCursor)
 except:
     print('connection: error *404*')
     exit(11)
 
-# https://server121.hosting.reg.ru/phpmyadmin/tbl_structure.php?db=u0114376_project1&table=profile
+# https://server121.hosting.reg.ru/phpmyadmin
