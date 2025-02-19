550f5b88fb95d94f1d682eca65a89f501afbcdcf
Chernika656 <levkutepov2008@gmail.com>
2025-02-19 10:34:32 +0300

    oot_diffusion.py

--- oot_diffusion.py
+++ oot_diffusion.py
@@ -1,8 +1,8 @@
-from gradio_client import Client, handle_file
+etfrom gradio_client import Client, handle_file
 
 
 def AI_CONVERT(photo1_path, photo2_path):
-    client = Client("levihsu/OOTDiffusion", hf_token='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')
+    client = Client("levihsu/OOTDiffusion", hf_token='secret')
     result = client.predict(
             vton_img=handle_file(photo1_path),
             garm_img=handle_file(photo2_path),
